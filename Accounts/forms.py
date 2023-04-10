c6e2f77a0b833b2bd6ca696e3cf89722119bbc2f
Михаил Гольдфарб <goldfarbm@mail.ru>
2023-04-10 23:05:31 +0500

    Чуть чуть пошаманил с формой

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -6,7 +6,23 @@ from Accounts.models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'phone_number')
+        fields = ('username', 'email', 'phone_number', 'spec' ,'password1', 'password2')
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+            'phone_number': None,
+            'spec': None
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # for fieldname in ['password2']:
+        self.fields['password2'].help_text = None
+        self.fields['password1'].help_text = None
+        self.fields['username'].help_text = None
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
