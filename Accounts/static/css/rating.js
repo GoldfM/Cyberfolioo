33e0c5e2025e33ec4f46e77fbb2d3cb88a22056d
Emma-Shifter <n.ba.04@mail.ru>
2023-04-26 18:24:04 +0500

    stars rating done

--- Accounts/static/css/rating.js
+++ Accounts/static/css/rating.js
@@ -1,28 +1,11 @@
-class Rating {
-  constructor(dom) {
-    dom.innerHTML = '<svg width="110" height="20"></svg>';
-    this.svg = dom.querySelector('svg');
-    for(var i = 0; i < 5; i++)
-      this.svg.innerHTML += `<polygon data-value="${i+1}"
-           transform="translate(${i*22},0)"
-           points="10,1 4,19.8 19,7.8 1,7.8 16,19.8" stroke-linejoin="round">`;
-    this.svg.onclick = e => this.change(e);
-    this.render();
-  }
+var stars = document.querySelectorAll('.star')
+                        document.addEventListener("click", function (e) {
+                        var index = e.target.dataset.value;
 
-  change(e) {
-    let value = e.target.dataset.value;
-    value && (this.svg.parentNode.dataset.value = value);
-    this.render();
-  }
-
-  render() {
-    this.svg.querySelectorAll('polygon').forEach(star =>
-    {
-      let on = +this.svg.parentNode.dataset.value >= +star.dataset.value;
-      star.classList.toggle('active', on);
-    });
-  }
-}
-
-document.querySelectorAll('.rating').forEach(dom => new Rating(dom));
+                        for (var i = 0; i < 5; ++i){
+                            stars[i].classList.remove('active')
+                        }
+                        for (var i = 0; i < index; ++i){
+                            stars[i].classList.add('active')
+                        }
+                        });
